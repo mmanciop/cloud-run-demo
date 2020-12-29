5f2575f05db8f3eed446a3809350628b4bc97a61
Michele Mancioppi <michele.mancioppi@gmail.com>
2020-12-29 15:05:29 +0100

    Add local file with URL of demo entry point

--- main.tf
+++ main.tf
@@ -318,3 +318,12 @@ resource "google_cloud_run_service_iam_policy" "dotnet_noauth" {
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
+
+resource "local_file" "entry_service_url" {
+  content = google_cloud_run_service.nodejs.status[0].url
+  filename = "entry_service_url"
+
+  depends_on = [
+    google_cloud_run_service.nodejs
+  ]
+}
